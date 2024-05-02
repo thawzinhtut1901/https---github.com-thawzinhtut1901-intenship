25d13152bd7c21b963b3e4104c89af29a10263e7
Thone Waddy Khin <139044080+thawzinhtut1901@users.noreply.github.com>
2024-05-01 22:55:58 -0700

    Update AdminDashboard.tsx

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -1,125 +1,159 @@
 import { useEffect, useState } from 'react';
 
+const Form = ({ onSubmit, formName, formPercentage }:any) => {
+  const [name, setName] = useState(formName || ''); 
+  const [percentage, setPercentage] = useState(formPercentage || ''); 
+
+  const handleSubmit = (event:any) => {
+    event.preventDefault();
+    onSubmit({ name, percentage: parseInt(percentage) });
+    setName('');
+    setPercentage('');
+  };
+
+  return (
+    <div className='flex flex-col mt-[340px] bg-sky-100'>
+      <form onSubmit={handleSubmit} className='flex flex-col my-6 justify-center items-center'>
+      <label className='flex m-4 text-lg'>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          className='ml-11 border-none  bg-slate-300 text-black rounded-md'
+        />
+      </label>
+
+      <label className='flex m-4 text-lg'>
+        Percentage:
+        <input
+          type="number"
+          value={percentage}
+          onChange={(e) => setPercentage(e.target.value)}
+          required
+          className='ml-1 border-black border-none bg-slate-300 text-black rounded-md'
+        />
+      </label>
+      <button className='text-lg bg-slate-400 text-black flex p-2 rounded-lg' type="submit">Add Collaborator</button>
+    </form>
+    </div>
+    
+  );
+};
+
 const AdminDashboard = () => {
   const [ownerPercentage, setOwnerPercentage] = useState(100);
-  const [collaborators, setCollaborators] = useState([
-    {id: 1, name: `Collaborator: ${1}`, percentage: 0 },
-    {id: 2, name: `Collaborator: ${2}`, percentage: 0 },
-  ]);
-
+  const [collaborators, setCollaborators] = useState<any[]>([]);
+  const [formName, setFormName] = useState('');
+  const [formPercentage, setFormPercentage] = useState('');
   const [unSaved, setUnSaved] = useState(false);
   const [isSaved, setIsSaved] = useState(false);
 
   useEffect(() => {
-    const handleBeforeUnsaved = (event:any) => {
-        if(unSaved) {
-            event.preventDefault(),
-            event.returnValue = '';
-        }
-    };
-    window.addEventListener('beforeunsaved', handleBeforeUnsaved);
-
-    return () => {
-      window.removeEventListener('beforeunsaved', handleBeforeUnsaved);
-    };
-  }, [unSaved]);  
-
-  const handlePercentageChange = (index: number, newValue: string) => {
-    if (/^\d*$/.test(newValue)) {
-      const newPercentage = newValue === '' ? 0 : parseInt(newValue);
-      if (newPercentage >= 0) {
-        const totalCollaboratorPercentage = collaborators.reduce((acc, curr) => acc + curr.percentage, 0);
-        const remainingPercentage = 100 - totalCollaboratorPercentage;
-    
-        if (newPercentage <= remainingPercentage) {
-          const newCollaborators = [...collaborators];
-          newCollaborators[index].percentage = newPercentage;
-          setCollaborators(newCollaborators);
-    
+      const handleBeforeUnsaved = (event:any) => {
+          if(unSaved) {
+              event.preventDefault(),
+              event.returnValue = '';
+          }
+      };
+      window.addEventListener('beforeunsaved', handleBeforeUnsaved);
   
-          const updatedOwnerPercentage = 100 - newCollaborators.reduce((acc, curr) => acc + curr.percentage, 0);
-          setOwnerPercentage(updatedOwnerPercentage);
-          setUnSaved(true);
-        } else {
-         
-          alert('Not enough remaining percentage for allocation.');
+      return () => {
+        window.removeEventListener('beforeunsaved', handleBeforeUnsaved);
+      };
+    }, [unSaved]);  
+
+    const handleSave = () => {
+          alert('Changes saved!');
+          setUnSaved(false);
+          setIsSaved(true);
         }
-      } else {
-        
-        alert('Input value cannot be less than zero.');
-      }
+
+  const handleAddCollaborator = (newCollaborator: any) => {
+    if (newCollaborator.percentage < 0) {
+      alert('Collaborator percentage cannot be negative.');
+      return;
+    }
+  
+    const totalCollaboratorPercentage = collaborators.reduce((acc, curr) => acc + curr.percentage, 0);
+  
+    const totalPercentageWithNewCollaborator = totalCollaboratorPercentage + newCollaborator.percentage;
+  
+    if (totalPercentageWithNewCollaborator > 100) {
+      alert('Cannot add collaborator. Total percentage exceeds 100%.');
+      return;
+    }
+  
+    const newCollaborators = [...collaborators, { ...newCollaborator }];
+    setCollaborators(newCollaborators);
+  
+    const updatedOwnerPercentage = 100 - totalPercentageWithNewCollaborator;
+    setOwnerPercentage(updatedOwnerPercentage);
+  
+    setFormName(newCollaborator.name);
+    setFormPercentage(newCollaborator.percentage.toString());
+  };
+  
+  const handleDeleteCollaborator = (index: number) => {
+    const deletedCollaborator = collaborators[index];
+    const updatedCollaborators = collaborators.filter((_, i) => i !== index);
+    const totalCollaboratorPercentage = updatedCollaborators.reduce((acc, curr) => acc + curr.percentage, 0);
+    const updatedOwnerPercentage = 100 - totalCollaboratorPercentage;
+
+    setCollaborators(updatedCollaborators);
+    setOwnerPercentage(updatedOwnerPercentage);
+
+    if (deletedCollaborator.name === formName) {
+      setFormName('');
+      setFormPercentage('');
     }
   };
 
-  const handleAddCollaborator = () => {
-    const newCollaboratorName = `Collaborator: ${collaborators.length + 1}`;
-    setCollaborators([...collaborators, {id: Date.now(), name: newCollaboratorName, percentage: 0 }]);
-  };
 
-  const removeCollaborator = (id:number) => {
-    const collaboratorToRemove = collaborators.find(collaborator => collaborator.id === id);
-   
-    if (collaboratorToRemove) {
-      
-      const updatedOwnerPercentage = ownerPercentage + collaboratorToRemove.percentage;
-      setOwnerPercentage(updatedOwnerPercentage);
-  
-      
-      const newCollaborators = collaborators.filter(collaborator => collaborator.id !== id);
-      setCollaborators(newCollaborators);
-    }
-  };
-
-  const handleSave = () => {
-    alert('Changes saved!');
-    setUnSaved(false);
-    setIsSaved(true);
-  }
   return (
-    <div className='bg-yellow-200 min-h-screen min-w-screen flex flex-col items-center justify-center'>
-      <h1 className="font-bold text-rose-600 text-4xl underline mb-5">ADMIN DASHBOARD</h1>
-      
+    <div>
+      <h1 className='flex flex-col items-center justify-center text-4xl border-b p-4 bg-slate-950 text-white'>Admin DashBoard</h1>
+    <div className='flex my-10 border-b items-center justify-between mx-32'>
+      <h2 className='text-2xl mb-2'>Thaw Zin Htut <span className="flex-1 m-1 absolute rounded bg-blue-600 text-sm font-thin text-white">Owner</span></h2>
+      <p className='text-2xl mb-2'>Percentage: <span>{ownerPercentage}%</span></p>
+    </div>
 
-      <div className="mb-5 w-96">
-        <h2 className="text-xl font-semibold mb-2">Owner</h2>
-        <p className="text-lg mb-2">Percentage: {ownerPercentage}%</p>
-      </div>
+    <div>
+      {collaborators.map((collaborator, index) => (
+        <div key={index} className='flex my-4 border-b items-center justify-between mx-32'>
+          <h3 className='text-2xl mb-2'>{collaborator.name} <span className='flex-1 m-1 absolute rounded bg-red-600 text-sm font-thin text-white'>New</span></h3>
+          
 
-      <div className="mb-5 w-96">
-        <h2 className="text-xl font-semibold mb-2">Collaborators</h2>
-        
-        {collaborators.map((collaborator, index) => (
-          <div key={index} className="flex items-center space-x-3 mb-3">
-            
-              <>
-                <p className="text-lg">{collaborator.name}</p>
-                
-                <input
-                  className="border-2 ml-3 border-solid border-slate-400 bg-yellow-200 rounded-md py-2 px-3 pr-10 focus:outline-none focus:border-blue-500"
-                  type="text" 
-                  value={collaborator.percentage !== 0 ? collaborator.percentage : ''}
-                  onChange={(e) => handlePercentageChange(index, e.target.value)}
-                  disabled={isSaved} 
-                />
-              
-                <button className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded focus:outline-none text-lg" onClick={() => removeCollaborator(collaborator.id)}>Remove</button>
-              </>
-      
+          <div>
+            <p className='text-2xl mb-2'>Percentage: <span>{collaborator.percentage}%</span></p>
+            <button className='bg-red-400 rounded ml-32 text-white p-1' onClick={() => handleDeleteCollaborator(index)}>Delete</button>   
           </div>
-        ))}
-      </div>
-      <div className='flex gap-4'>
-        <button className='bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded focus:outline-none text-lg' onClick={handleAddCollaborator}>Add Collaborator</button>
+          
+        </div>
+        
+      ))}
+
+       
+    </div> 
+    
+      <div className='flex justify-end mr-28'>
         <button
-          className={`bg-${unSaved ? 'red ' : 'blue'}-500 hover:bg-${unSaved ? 'red' : 'blue'}-600 px-3 bg-red-500 hover:bg-red-600 text-white py-2 rounded focus:outline-none text-lg`}
+          className={`bg-${unSaved ? 'red ' : 'blue'}-500 hover:bg-${unSaved ? 'red' : 'blue'}-600 px-3 bg-blue-400 hover:bg-blue-500 text-white py-2 rounded focus:outline-none text-lg`}
           onClick={handleSave}
         >
           {unSaved ? 'Unsaved' : 'Save'}
         </button>
       </div>
-    </div>
-  );
-};
+    {/* Form for adding new collaborator */}
+    <Form
+      onSubmit={handleAddCollaborator}
+      formName={formName}
+      formPercentage={formPercentage}
+      disabled={isSaved}
+    />
+  </div>
+  )
+}
 
 export default AdminDashboard;
-
